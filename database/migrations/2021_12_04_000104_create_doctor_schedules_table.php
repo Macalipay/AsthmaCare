eadffe59550214a69972ebe064aa134a34a51ab7
Arvin Olivas <arvin.olivas15@gmail.com>
2021-12-04 00:06:31 +0800

    commit change

---
+++ database/migrations/2021_12_04_000104_create_doctor_schedules_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDoctorSchedulesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('doctor_schedules', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('doctor_id');
+            $table->string('day');
+            $table->string('status');
+            $table->timestamps();
+            
+            $table->foreign('doctor_id')
+                ->references('id')
+                ->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('doctor_schedules');
+    }
+}
